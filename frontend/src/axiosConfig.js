2f349bd86e6f42dd995a8d413ade11681e9f0662
shubhannaik-droid <shubhan.naik@gmail.com>
2026-04-04 17:21:08 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://smart-grocery-app-slen.onrender.com/api",
+  baseURL: "https://smart-grocery-app-slen.onrender.com",
 });
 
 export default instance;
